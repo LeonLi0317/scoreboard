137e6f5bb46d33d1f58536cf4272270de2349010
李振主 <LEON0317@HFCFinance.com.tw>
2024-11-18 17:07:16 +0800

    import PWA TEST

---
+++ script/service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-pwa-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/index.css',
+                '/script/index.js',
+                '/image/favicon.ico',
+                '/image/favicon.ico'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
